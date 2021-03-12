051a3272bcacf0ed59d32d3d514490755a5f7e78
Lean <l.m.gomez@outlook.com>
2021-03-11 22:04:33 -0300

    Add Project structure
    add project structure

---
+++ MeLiChallenge.Model/Satellite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MeLiChallenge.Model
+{
+    public class Satellite
+    {
+        [Key]
+        public int IdSatelite { get; set; }
+        public string Name { get; set; }
+        public Position Position { get; set; }
+    }
+}
